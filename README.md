47f06a5452b9de30213431aeabfdc7f3deb648a3
fan.liu <fan.liu@szscnkj.cn>
2024-12-07 10:19:45 +0800

    yearRange

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-"# vue-component" 
+# 多有借鉴
+  多有参考和借鉴
+# 组件
+## year-picker
+element-ui DatePicker 年范围的选择组件，参数类同
+```
+      <year-picker
+        v-model="yearRange"
+        value-format="yyyy"
+        type="yearrange"
+        range-separator="-"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+      />
+      yearRange: ['2024',2024]
+```
---
+++ year-picker/year-picker.js
@@ -0,0 +1,34 @@
+import Picker from './picker';
+import YearRangePanel from './year-range';
+
+const getPanel = function(type) {
+    return YearRangePanel;
+};
+export default {
+  mixins: [Picker],
+
+  name: 'yearPicker',
+
+  props: {
+    type: {
+      type: String,
+      default: 'date'
+    },
+    timeArrowControl: Boolean
+  },
+
+  watch: {
+    type(type) {
+      if (this.picker) {
+        this.unmountPicker();
+        this.panel = YearRangePanel;
+        this.mountPicker();
+      } else {
+        this.panel = YearRangePanel;
+      }
+    }
+  },
+  created() {
+    this.panel = YearRangePanel;
+  }
+};
